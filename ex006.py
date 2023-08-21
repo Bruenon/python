5f7a9cf111a420cd7de5af387aea15d3501a755e
Brueno <bruno_mennabkc@hotmail.com>
2023-08-21 20:59:31 -0300

    Exercícios 5 a 7

---
+++ ex006.py
@@ -0,0 +1,5 @@
+num = int(input('Digite um número: '))
+d = num * 2
+t = num * 3
+r = num ** (1/2)
+print(f'O dobro de {num} vale {d}.\nO triplo de {num} vale {t}.\nA raiz quadrada de {num} é igual a {r:.2f}.')
